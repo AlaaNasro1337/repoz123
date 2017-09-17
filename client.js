f6eb45c2b69212ddc2ffd4f7f56bb491956148a7
Stephen Falcone <stephenfalcone3@gmail.com>
2017-09-17 13:39:13 -0400

    first commit. remember to node install on redownload

---
+++ client.js
@@ -0,0 +1,54 @@
+//waits until page is ready for javascript
+$(function() {
+
+//allows socket connections
+var socket = io();
+    console.log('Connected to socket...');
+          
+    //sign in button enter and button press
+    $("#username").keyup(function(event){
+        if(event.keyCode == 13){                                    //if enter key pressed
+            $("#signInBtn").click();
+        }
+    });
+    $('#signInBtn').click(function(){
+        socket.emit('name input', $("#username").val());            //Send 'name input' to server
+        $(this).prop("disabled",true);                              //Disable text box and button after sent
+        $("#username").prop("disabled",true); 
+        $('#textarea').focus();                                     //change focus to #textarea  
+    });
+    
+    //send 'message sent' to server
+    $('#textarea').keypress(function(event){
+        if(event.keyCode == 13 ){                                     // if enter key pressed 
+            event.preventDefault();                                   //prevent next line on enter key press
+            socket.emit('message sent', $("#textarea").val());        //send message to server
+            $("#textarea").val("");                                   //clear textbox after msg sent
+        }
+    });
+
+    
+    //When server emits 'new message', update chat body
+    socket.on('new message', function(data){
+        $('<div/>').text(data.username+":  " +data.message).appendTo($('#chat-area'));      //creates new div with text and appends to 'chat-area' div
+        $('#chat-area')[0].scrollTop = $('#chat-area')[0].scrollHeight;                     //sets chat area to stay scrolled down
+    });
+
+    //reloads when disconnected from server
+    socket.on('disconnect', function(){
+        console.log('Disconnected from socket...');
+        location.reload();
+    });
+
+    //displays old messages from mongo server
+    socket.on('display old messages', function(res){
+        if(res.length){
+            for(var x=0; x<res.length; x++){
+                $('<div/>').text(res[x].name+":  " +res[x].message).prependTo($('#chat-area'));
+                $('#chat-area')[0].scrollTop = $('#chat-area')[0].scrollHeight;
+                //console.log(res[x]);
+            }
+        }
+    });
+          
+});
---
+++ server.js
@@ -0,0 +1,69 @@
+// Setup basic express server
+var express = require('express');
+var app = express();
+const mongo = require('mongodb').MongoClient;           //MONGODB
+var path = require('path');
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var port = process.env.PORT || 4000;                    //PORT=XXXX node server.js will run on port XXXX
+
+//Set server to given port or 4000 if none given.
+server.listen(port, function(){
+    console.log('Server listening at port %d', port);
+}); 
+
+//Routing. Allows access to index.html, style.css, etc.
+app.use(express.static(path.join(__dirname, '/')));
+
+//Connect to MongoDB
+mongo.connect('mongodb://127.0.0.1/anonchat', function(err, db){
+    if(err){
+        throw err;
+    }
+    console.log('MongoDB connected...');
+   
+//When a user enters webpage
+io.on('connection', function(socket){
+    console.log('An unknown user has connected.');
+    let chat = db.collection('chats');
+
+      //Get the last 20 messages from MongoDB
+      chat.find().limit(20).sort({_id:-1}).toArray(function(err, res){
+        if(err){
+            throw err;
+        }
+
+        //Emit the previous messages from DB
+        socket.emit('display old messages',res);
+    });
+
+    //When a user leaves webpage
+    socket.on('disconnect', function(){
+        console.log('An unknown user has disconnected.');
+    });
+
+    //When a user signs in
+	socket.on('name input', function(name){
+	    if (name != ''){
+   	        console.log(name + " has signed in.");
+        }
+        //when user sends message
+        socket.on('message sent', function(msg){
+            if (msg != ''){
+                console.log(name + ": " + msg);
+                chat.insert({name: name, message: msg}, function(){
+                    io.emit('new message', {
+                        username: name,
+                        message: msg
+                    });
+                });
+            }
+        });
+
+        //when signed in user leaves
+        socket.on('disconnect', function(){
+            console.log(name+" has disconnected.");
+        });
+    });
+});
+});
